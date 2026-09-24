801509fc7a67f4a629034ef010ec8ade1a05ca93
joshuagupit0 <98499408+joshuagupit0@users.noreply.github.com>
2026-09-25 01:30:21 +0800

    Add files via upload

---
+++ Hopecleaningco-main/js/terms.js
@@ -0,0 +1,24 @@
+
+    const popupOverlay = document.getElementById("popupOverlay");
+    const acceptBtn = document.getElementById("acceptBtn");
+    const declineBtn = document.getElementById("declineBtn");
+
+    //Pop-terms
+    document.addEventListener("click", function(event) {
+    if (event.target.id === "bookBtn" || event.target.id === "bookBtn1" || event.target.id === "bookBtn2") {
+      popupOverlay.style.display = "flex";
+    }
+  });
+
+    // Accept terms
+    acceptBtn.addEventListener("click", () => {
+      popupOverlay.style.display = "none";
+      alert("Thank you for accepting! Redirecting to booking...");
+      window.location.href = "booking.html"; // replace with your booking page
+    });
+
+    // Decline terms
+    declineBtn.addEventListener("click", () => {
+      popupOverlay.style.display = "none";
+      alert("You must accept the Terms to proceed.");
+    });
